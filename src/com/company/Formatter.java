0d0c50930b9fdda80d9af4b19fb7ca6a8eeac1f2
tvakhrameeva <tatiana.vakhrameeva@gmail.com>
2016-10-20 12:17:11 -0700

    Add first version of  formatter

---
+++ src/com/company/Formatter.java
@@ -0,0 +1,52 @@
+package com.company;
+
+class Formatter {
+
+    private static int SpaceCount = 4;
+
+    static String Format(String code)
+    {
+        StringBuilder builder = new StringBuilder(code.replace(" ", ""));
+
+        int nestedLevel = 0;
+
+        for(int i=0;i<builder.length();i++)
+        {
+            if (builder.charAt(i)=='{') {
+                nestedLevel += 1;
+
+                if (i < (builder.length() - 1) && builder.charAt(i+1) != '\n') {
+                    builder.insert(i + 1, '\n');
+                    i++;
+                    continue;
+                }
+            }
+
+            if (builder.charAt(i)=='}') {
+                nestedLevel -= 1;
+
+                if (i > 0 && builder.charAt(i - 1) != '\n') {
+                    builder.insert(i, '\n');
+                    i++;
+                }
+
+                if (i < (builder.length()-1) && builder.charAt(i+1) != '\n') {
+                    builder.insert(i+1, '\n');
+                    i++;
+                }
+
+                continue;
+            }
+
+            if (builder.charAt(i) == ';' && (i < builder.length() - 1) && builder.charAt(i + 1) != '\n')
+            {
+                builder.insert(i + 1, '\n');
+                i++;
+            }
+
+        }
+
+        return builder.toString();
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String testCode = "{ command1; { command2; } command3;}";
+
+        System.out.println("Given code:\n" + testCode);
+        System.out.println("Code after formatting:\n" + Formatter.Format(testCode));
     }
 }
